604347bb3d0f74ca1d3690d8bf78c9c337759f9a
Sergi Pasoev <s.pasoev@gmail.com>
2016-01-09 20:24:00 +0400

    merge

--- app/src/main/java/com/example/godot/katranlate/domain/Language.java
+++ app/src/main/java/com/example/godot/katranlate/domain/Language.java
@@ -4,6 +4,7 @@ package com.example.godot.katranlate.domain;
  * Created by sergi on 09/01/16.
  */
 public class Language {
+    private int id;
     private String iso;
     private String name;
 }
