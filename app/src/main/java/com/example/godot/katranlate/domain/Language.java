c5af2deaa018b524ea18f38a471df2ffed189c2e
Sergi Pasoev <s.pasoev@gmail.com>
2016-01-09 20:24:36 +0400

    merge

--- app/src/main/java/com/example/godot/katranlate/domain/Language.java
+++ app/src/main/java/com/example/godot/katranlate/domain/Language.java
@@ -7,4 +7,34 @@ public class Language {
     private int id;
     private String iso;
     private String name;
+
+    public Language(int id, String iso, String name) {
+        this.id = id;
+        this.iso = iso;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIso() {
+        return iso;
+    }
+
+    public void setIso(String iso) {
+        this.iso = iso;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
